e258a91599c73e6721432fad2fd42410aeb66df0
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2021-09-29 19:36:26 -0300

    finalizado a vinculação de dados e a formatação dos valores para exibição correta no fragment de detalhe.

--- app/src/main/java/com/example/android/marsrealestate/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/detail/DetailViewModel.kt
@@ -17,10 +17,8 @@
 package com.example.android.marsrealestate.detail
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.example.android.marsrealestate.R
 import com.example.android.marsrealestate.network.MarsProperty
 
 /**
@@ -31,6 +29,25 @@ class DetailViewModel(@Suppress("UNUSED_PARAMETER")marsProperty: MarsProperty, a
     private val _selectedProperty = MutableLiveData<MarsProperty>()
     val selectedProperty: LiveData<MarsProperty>
     get() = _selectedProperty
+
+    val displayPropertyPrice = Transformations.map(selectedProperty) {
+        app.applicationContext.getString(
+            when(it.isRent){
+                true -> R.string.display_price_monthly_rental
+                else -> R.string.display_price
+            }, it.price
+        )
+    }
+
+    val displayPropertyType = Transformations.map(selectedProperty){
+        app.applicationContext.getString(R.string.display_type,
+            app.applicationContext.getString(
+                when(it.isRent){
+                    true -> R.string.type_rent
+                    false -> R.string.type_sale
+                }
+            ))
+    }
 
     init {
         _selectedProperty.value = marsProperty
